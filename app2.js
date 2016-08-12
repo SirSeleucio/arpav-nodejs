27fdffb469b8080c63d1ced7168a8b66ed77da5c
Cristiano Colpo <cristianocolpo@gmail.com>
2016-08-12 11:24:54 +0200

    aggiornamento finale

--- app2.js
+++ app2.js
@@ -6,20 +6,20 @@ var db = new sqlite3.Database('database1.db');
 //#ariadativalidati > table > tbody > tr:nth-child(6) > td:nth-child(3)
 //
 date = {
-  mese: ['01','02','03','04','05','06','07','08','09','10','11','12'],
-  anno: '2011'
+  giorno: ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30'],
+  anno: '2015'
 }
 
-for (var i = 0; i < date.mese.length; i++) {
+for (var i = 0; i < date.giorno.length; i++) {
   var index = i;
   (function (index) {
     request.post({
       url:'http://www.arpa.veneto.it/arpavinforma/bollettini/aria/aria_dati_validati_storico.php',
       form: {
         provincia:'vicenza',
-        giorno: '01',
-        mese: date.mese[i],
-        anno: '2011',
+        giorno: date.giorno[i],
+        mese: '07',
+        anno: date.anno,
         Vari:'Visualizza il bollettino'
       }
     }, function(err,httpResponse,body,i){
@@ -38,26 +38,21 @@ for (var i = 0; i < date.mese.length; i++) {
         var o3_bassano = $('table tr:nth-child(13) td:nth-child(10)', '#ariadativalidati').text();
       }
       db.serialize(function () {
-        db.run("INSERT or IGNORE INTO Bassano (tipoMisurazione,dataMisurazione,misurazione) VALUES ('NO2','2011-"+date.mese[index]+"-01 "+ora_bassano+":00:00','"+no2_bassano+"');");
+        db.run("INSERT or IGNORE INTO Bassano (tipoMisurazione,dataMisurazione,misurazione) VALUES ('NO2','"+date.anno+"-05-"+date.giorno[index]+" "+ora_bassano+":00:00','"+no2_bassano+"');");
         //db.run("INSERT INTO DatiBis(idStazione, tipoMisurazione, dataMisurazione, Misurazione) VALUES('"+trueStationCode+"', 'OZONO', '"+dataMisurazione+"','"+misurazione+"')");
       });
       db.serialize(function () {
-        db.run("INSERT or IGNORE INTO Bassano (tipoMisurazione,dataMisurazione,misurazione) VALUES ('03','2011-"+date.mese[index]+"-01 "+ora_bassano+":00:00','"+o3_bassano+"');");
+        db.run("INSERT or IGNORE INTO Bassano (tipoMisurazione,dataMisurazione,misurazione) VALUES ('O3','"+date.anno+"-05-"+date.giorno[index]+" "+ora_bassano+":00:00','"+o3_bassano+"');");
         //db.run("INSERT INTO DatiBis(idStazione, tipoMisurazione, dataMisurazione, Misurazione) VALUES('"+trueStationCode+"', 'OZONO', '"+dataMisurazione+"','"+misurazione+"')");
-      });
-      db.serialize(function () {
-        db.run("INSERT or IGNORE INTO Asiago (tipoMisurazione,dataMisurazione,misurazione) VALUES ('NO2','2011-"+date.mese[index]+"-01 "+ora_asiago+":00:00','"+no2_asiago+"');")
-      });
-      db.serialize(function () {
-        db.run("INSERT or IGNORE INTO Asiago (tipoMisurazione,dataMisurazione,misurazione) VALUES ('03','2011-"+date.mese[index]+"-01 "+ora_asiago+":00:00','"+o3_asiago+"');")
       });
       /*
-      console.log(no2_asiago);
-      console.log(ora_asiago);
-      console.log(no2_bassano);
-      console.log(ora_bassano);
+      db.serialize(function () {
+        db.run("INSERT or IGNORE INTO Asiago (tipoMisurazione,dataMisurazione,misurazione) VALUES ('NO2','"+date.anno+"-"+date.mese[index]+"-01 "+ora_asiago+":00:00','"+no2_asiago+"');")
+      });
+      db.serialize(function () {
+        db.run("INSERT or IGNORE INTO Asiago (tipoMisurazione,dataMisurazione,misurazione) VALUES ('03','"+date.anno+"-"+date.mese[index]+"-01 "+ora_asiago+":00:00','"+o3_asiago+"');")
+      });
       */
-      console.log();
     });
   })(i);
 }
